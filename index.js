e88658830bfb0b1c433e2e032ce1a74687a008de
Syha Ban <syhaban@hotmail.com>
2023-04-16 20:39:54 -0400

    Adds index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function receivesAFunction(callback){
+    console.log(callback())
+};
+function returnsANamedFunction(){
+    console.log("Hello")
+    return returnsANamedFunction
+};
+function returnsAnAnonymousFunction(){
+    return function() {"Anonymous"}
+};
